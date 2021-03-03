3554a65f6692632c25fa23d8baffe931e64e511c
Gyles Polloso <gyles.polloso@fairfaxmedia.co.nz>
2021-03-03 21:07:45 +1300

    Try using tesseract extract tess resources

--- src/main/java/com/think/ocr/service/OcrService.java
+++ src/main/java/com/think/ocr/service/OcrService.java
@@ -2,6 +2,7 @@ package com.think.ocr.service;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,6 +10,7 @@ import java.util.stream.Collectors;
 import com.think.ocr.domain.TextContent;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -36,7 +38,7 @@ public class OcrService {
 		try {
 			BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(image.getBytes()));
 			Tesseract tesseract = new Tesseract();
-			tesseract.setDatapath("src/main/resources/tessdata");
+			tesseract.setDatapath(LoadLibs.extractTessResources("tessdata").getPath());
 			tesseract.setLanguage("eng");
 			tesseract.setPageSegMode(1);
 			tesseract.setOcrEngineMode(1);
